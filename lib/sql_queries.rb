616505564c26d20bdf4e1963c4ce1b11602c291d
GLADYS <mungaiglado106@gmail.com>
2022-08-20 20:39:09 +0300

    commit tree 3a3d677d7825225887c96eb537a215e6b21c4599

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,17 +4,17 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT bears.name, FROM bears ORDERED BY ASC;"
+  "SELECT bears.name, FROM bears ORDER BY ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age WHERE alive = 1 ORDERED BY bears.age ASC;"
+  "SELECT bears.name, bears.age WHERE alive = 1 ORDER BY bears.age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDERED BY bears.age DESC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDERED BY bears.age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;"
 end
