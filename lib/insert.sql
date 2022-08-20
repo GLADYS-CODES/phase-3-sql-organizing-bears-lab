b2b6d33d398f63b4bfd2d5f4316fe79684f11a96
GLADYS <mungaiglado106@gmail.com>
2022-08-20 20:25:51 +0300

    intial commit

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Mr. Chocolate', 36, 'M', 'black', 'yes', 'true');
+
+
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Rowdy', 38, 'F', 'black', 'yes', 'true');
+
+
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Tabitha', 34, 'F', 'black', 'yes', 'true');
+
+
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Sergeant', 32, 'F', 'black', 'yes', 'true');
+
+
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Melissa', 31, 'F', 'black', 'yes', 'true');
+
+INSERT INTO bears (name, age, sex, color, temperature, alive) VALUES ('Grinch', 35, 'M', 'black', 'yes', 'true');
+
+
+INSERT INTO bears (name) VALUES (NULL);
